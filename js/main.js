e7464dddc1f911f544a9b818fd44a415c3857c6e
viniciusmdias <vinicius-mdias@hotmail.com>
2018-07-26 18:55:37 -0300

    Small changes

--- js/main.js
+++ js/main.js
@@ -1,30 +1,11 @@
 /* this event is change to css propierty -> scroll-behavior. */
 
-var scroll = function (selector) {
-
-    $(selector).click(function (event) {
-
-
-        console.log("fui clicado");
-        
-        event.preventDefault();
-        var target = $(this).attr('href');
-
-        $('html, body').animate ({
-            scrollTop: $(target).offset().top
-
-        }, 1000)
-    });
-
-}
 $(document).scroll(function() {
     
     $("header").addClass('navbarMove').fadeIn();
     if($(this).scrollTop() == 0) {
         $("header").removeClass('navbarMove');
     }
-
-
 });
 
 
