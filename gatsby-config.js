4ce1851445931b479b1f46d68394a405338a919e
viniciusmdias <vinicius-mdias@hotmail.com>
2020-03-18 15:49:37 -0300

    Add site to google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,13 @@ module.exports = {
 				icon: "static/favicon.svg",
 			},
 		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				// replace "UA-XXXXXXXXX-X" with your own Tracking ID
+				trackingId: config.googleAnalyticsID,
+			},
+		},
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
