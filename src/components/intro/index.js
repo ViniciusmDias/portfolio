46346599c04a2f1501708733bc6655074b115ce2
viniciusmdias <vinicius-mdias@hotmail.com>
2020-03-29 00:59:04 -0300

    Add lights on in background intro

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -5,13 +5,15 @@ import Button from "../button"
 export default function Intro() {
 	return (
 		<>
-			<div className="container intro">
-				<h1>Hi, I'm Vinícius a Frontend Developer.</h1>
-				<p>
-					I have over 3 years experience developing websites and applications
-					with HTML, CSS, JS and React.
-				</p>
-				<Button title="technologies" />
+			<div className="background">
+				<div className="container intro">
+					<h1>Hi, I'm Vinícius a Frontend Developer.</h1>
+					<p>
+						I have over 3 years experience developing websites and applications
+						with HTML, CSS, JS and React.
+					</p>
+					<Button title="technologies" />
+				</div>
 			</div>
 		</>
 	)
