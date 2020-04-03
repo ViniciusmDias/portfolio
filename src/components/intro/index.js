bc5b7d90953e63f67ea6d0e0bd91749c65b1e759
viniciusmdias <vinicius-mdias@hotmail.com>
2020-04-02 22:27:30 -0300

    Fix some bugs

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -5,15 +5,13 @@ import Button from "../button"
 export default function Intro() {
 	return (
 		<>
-			<div className="background">
-				<div className="container intro">
-					<h1>Hi, I'm Vinícius a Frontend Developer.</h1>
-					<p>
-						I have over 3 years experience developing websites and applications
-						with HTML, CSS, JS and React.
-					</p>
-					<Button title="technologies" />
-				</div>
+			<div className="container intro">
+				<h1>Hi, I'm Vinícius a Frontend Developer.</h1>
+				<p>
+					I have over 3 years experience developing websites and applications
+					with HTML, CSS, JS and React.
+				</p>
+				<Button title="technologies" />
 			</div>
 		</>
 	)
